d454ea6fb82475d3f3451db7091bb2acc5cf84be
DeuxEx <per@wennergrund.net>
2026-07-19 15:24:58 +0200

    Update start-stream.sh with new streaming options

--- start-stream.sh
+++ start-stream.sh
@@ -1,5 +1,5 @@
-#script to start twitchstream on capturedevice
 
+#script to start twitchstream on capturedevice
 #run this cmd to bypass authentication every time
 #gsr info: gsr_kms_client_init: gsr-kms-server is missing sys_admin cap and will require root authentication.
 
@@ -30,14 +30,24 @@ videocodec="h264"
 #quality: medium, high, very_high, ultra for vbr or bitrate for cbr.
 quality="8000"
 
+
 frameratemode="vfr"
 colorrange="limited"
 framerate="60"
 
+#add text to stream?
+#ffmpeg -i input.mp4 -vf "drawtext=fontfile=/path/to/font.ttf:textfile=text.txt:reload=1:fontcolor=white:fontsize=24:box=1:boxcolor=black@0.5:boxborderw=5:x=(w-text_w)/2:y=(h-text_h)/2" -codec:a copy output.mp4
+startposx="200"
+startposy="200"
+#default; boxborderw=5:x=(w-text_w)/2:y=(h-text_h)/2
+textfield="drawtext=fontfile=/usr/share/fonts/TTF/ZillaSlab-Regular.ttf:textfile=streamtext.txt:reload=1:fontcolor=white:fontsize=24:box=1:boxcolor=black@0.5:boxborderw=5:x=$startposx:y=$startposy"
+
+
+
 #-k codec
-# Video codec: auto, h264, hevc, av1, vp8, vp9, hevc_hdr, av1_hdr,
-# hevc_10bit, av1_10bit (default: auto → h264). HDR options not
-# available on X11 or portal capture.
+#              Video codec: auto, h264, hevc, av1, vp8, vp9, hevc_hdr, av1_hdr,
+#              hevc_10bit, av1_10bit (default: auto → h264). HDR options not
+#              available on X11 or portal capture.
 
 
 
@@ -46,28 +56,27 @@ framerate="60"
 # -i input file url
 # -i - = standard input
 
+#output to file
+#$ gpu-screen-recorder -w portal -o [path/to/video.mp4]
 
 
 #gpu-screen-recorder -w $capturedevice -c $videocodec -s $resolution -bm cbr -q $quality -encoder gpu -ab $audiobitrate  -f $framerate -a default_output |
 # ffmpeg -i - -f mpegts -c:a copy -f flv $platform/$streamkey
 
 
-#output to file
+#dump till file
 #gpu-screen-recorder -ab $audiobitrate -w $capturedevice -c $format -s $resolution -bm cbr -q $quality -ac $audiocodec -cursor no -cr $colorrange -k $videocodec -encoder gpu -f $framerate -a default_output -restore-portal-session yes -o /home/void/testrun.mp4
 
-
-#stream to twitch
-#Use ffmpeg to stream to an RTMP server, continue processing the stream at real-time rate even in case of temporary failure (network outage) and attempt to recover streaming every second indefinitely:
-gpu-screen-recorder -ab $audiobitrate -w $capturedevice -c $format -s $resolution -bm cbr -q $quality -ac $audiocodec -cursor no -cr $colorrange -k $videocodec -encoder gpu -f $framerate -a default_output -restore-portal-session yes | ffmpeg -re -i - -c:v libx264 -c:a aac -f fifo -fifo_format flv -drop_pkts_on_overflow 1 -attempt_recovery 1 -recovery_wait_time 1 -map 0:v -map 0:a $platform/$keyvalue
-
+#stream till twitch
+gpu-screen-recorder -ab $audiobitrate -w $capturedevice -c $format -s $resolution -bm cbr -q $quality -ac $audiocodec -cursor no -cr $colorrange -k $videocodec -encoder gpu -f $framerate -a default_output -restore-portal-session yes | ffmpeg -re -i - -c:v h264_nvenc -c:a aac -threads 3 -vf $textfield -flags:v +global_header -g $framerate -bufsize 6000k -f fifo -fifo_format flv -drop_pkts_on_overflow 1 -attempt_recovery 1 -recovery_wait_time 1 -map 0:v -map 0:a $platform/$keyvalue
 
 #stop the stream after stop with ctrl-z
 killall ffmpeg
 
+# -re = read input frame rate
+#-c:v libx264, h264_nvenc
+#-c:a aac
 
 #testsignal
 #ffmpeg -re -f lavfi -i testsrc2=size=$resolution -f lavfi -i aevalsrc="sin(0*2*PI*t)" -vcodec libx264 -r 30 -g 30 -preset fast -vb 3000k -pix_fmt rgb24 -pix_fmt yuv420p -f flv $platform/$keyvalue
-
-
-
 
